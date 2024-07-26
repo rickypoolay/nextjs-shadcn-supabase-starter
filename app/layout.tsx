89e5c122b4e226ede9db0394323482d642599505
Ricky <rickypoullet@gmail.com>
2024-07-26 00:49:00 -0400

    Initial

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,14 +1,19 @@
-import { GeistSans } from "geist/font/sans";
-import "./globals.css";
+import { GeistSans } from 'geist/font/sans';
+import './globals.css';
+import Navbar from '@/components/Navbar';
+import { ThemeProvider } from 'next-themes';
+import Footer from '@/components/Footer';
+import { createClient } from '@/utils/supabase/client';
 
 const defaultUrl = process.env.VERCEL_URL
   ? `https://${process.env.VERCEL_URL}`
-  : "http://localhost:3000";
+  : 'http://localhost:3000';
 
 export const metadata = {
   metadataBase: new URL(defaultUrl),
-  title: "Next.js and Supabase Starter Kit",
-  description: "The fastest way to build apps with Next.js and Supabase",
+  title: 'Next.js, Shadcn, and Supabase Starter Kit',
+  description:
+    'The fastest way to build apps with Next.js, Shadcn and Supabase',
 };
 
 export default function RootLayout({
@@ -17,11 +22,13 @@ export default function RootLayout({
   children: React.ReactNode;
 }) {
   return (
-    <html lang="en" className={GeistSans.className}>
-      <body className="bg-background text-foreground">
-        <main className="min-h-screen flex flex-col items-center">
-          {children}
-        </main>
+    <html lang='en' className={GeistSans.className}>
+      <body className='bg-background text-primary min-h-[100vh] flex flex-col'>
+        <ThemeProvider attribute='class' defaultTheme='system' enableSystem>
+          <Navbar />
+          <main className='flex-1 flex'>{children}</main>
+        </ThemeProvider>
+        <Footer />
       </body>
     </html>
   );
--- app/page.tsx
+++ app/page.tsx
@@ -1,54 +1,29 @@
-import DeployButton from "../components/DeployButton";
-import AuthButton from "../components/AuthButton";
-import { createClient } from "@/utils/supabase/server";
-import ConnectSupabaseSteps from "@/components/tutorial/ConnectSupabaseSteps";
-import SignUpUserSteps from "@/components/tutorial/SignUpUserSteps";
-import Header from "@/components/Header";
+import SectionContentContainer from '@/components/PageContainer';
+import { Button } from '@/components/ui/button';
+import { cn } from '@/lib/utils';
 
 export default async function Index() {
-  const canInitSupabaseClient = () => {
-    // This function is just for the interactive tutorial.
-    // Feel free to remove it once you have Supabase connected.
-    try {
-      createClient();
-      return true;
-    } catch (e) {
-      return false;
-    }
-  };
-
-  const isSupabaseConnected = canInitSupabaseClient();
-
   return (
-    <div className="flex-1 w-full flex flex-col gap-20 items-center">
-      <nav className="w-full flex justify-center border-b border-b-foreground/10 h-16">
-        <div className="w-full max-w-4xl flex justify-between items-center p-3 text-sm">
-          <DeployButton />
-          {isSupabaseConnected && <AuthButton />}
-        </div>
-      </nav>
+    <SectionContentContainer>
+      <div className='mt-4 md:mt-14'>
+        <h1 className='text-5xl md:w-3/4 md:text-left md:text-7xl text-center'>
+          Hello World,
+          <br /> welcome to the home page.
+        </h1>
 
-      <div className="flex-1 flex flex-col gap-20 max-w-4xl px-3">
-        <Header />
-        <main className="flex-1 flex flex-col gap-6">
-          <h2 className="font-bold text-4xl mb-4">Next steps</h2>
-          {isSupabaseConnected ? <SignUpUserSteps /> : <ConnectSupabaseSteps />}
-        </main>
-      </div>
-
-      <footer className="w-full border-t border-t-foreground/10 p-8 flex justify-center text-center text-xs">
-        <p>
-          Powered by{" "}
-          <a
-            href="https://supabase.com/?utm_source=create-next-app&utm_medium=template&utm_term=nextjs"
-            target="_blank"
-            className="font-bold hover:underline"
-            rel="noreferrer"
-          >
-            Supabase
-          </a>
+        <p className='mt-5 md:w-3/4'>
+          Lorem ipsum dolor sit amet consectetur, adipisicing elit. Et
+          repudiandae reprehenderit, omnis earum exercitationem eius doloribus
+          beatae laboriosam? Modi, sequi?
         </p>
-      </footer>
-    </div>
+
+        <div className='flex gap-5 mt-10 items-center mx-auto justify-center md:justify-start'>
+          <Button variant={'secondary'} size={'lg'}>
+            This
+          </Button>
+          <Button size={'lg'}>That</Button>
+        </div>
+      </div>
+    </SectionContentContainer>
   );
 }
